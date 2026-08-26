b7d5bb9ec62c635ed084b21d3eda6a4cf60566b7
Boba <bambukvova3@gmail.com>
2026-08-26 10:43:22 +0100

    Add files via upload

---
+++ JSW/out.js
@@ -0,0 +1,10 @@
+function output(text_output) { console.log(text_output); }
+function input(text_input) { prompt(text_input); }
+function ret(status_return) { return status_return; }
+function alert(text_alert) { window.alert(text_alert); }
+function opn_wind(name_window) { window.location.href = name_window; }
+function goto_wind(name_window2) { window.open(name_window2); }
+function Button(text_button) { <button>{ text_button }</button> }
+function Input(text_input) { <input> { text_input }</input> }
+function ext(status_ext) { exit(status_ext); }
+function fl_btn() { <input type="file"></input> }
