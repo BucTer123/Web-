ef173cfa0cd7d81da812f872648310aebb2042fa
Boba <bambukvova3@gmail.com>
2026-08-27 09:23:24 +0200

    Update out.js

--- JSW/out.js
+++ JSW/out.js
@@ -1,10 +1,11 @@
-function output(text_output) { console.log(text_output); }
-function input(text_input) { prompt(text_input); }
-function ret(status_return) { return status_return; }
-function alert(text_alert) { window.alert(text_alert); }
-function opn_wind(name_window) { window.location.href = name_window; }
-function goto_wind(name_window2) { window.open(name_window2); }
-function Button(text_button) { <button>{ text_button }</button> }
-function Input(text_input) { <input> { text_input }</input> }
-function ext(status_ext) { exit(status_ext); }
-function fl_btn() { <input type="file"></input> }
+function OutputCMD(text_output) { console.log(text_output); }
+function InputCMD(text_input) { prompt(text_input); }
+function ReturnCMD(status_return) { return status_return; }
+function AlertHTML(text_alert) { window.alert(text_alert); }
+function Open_windowHTML(name_window) { window.location.href = name_window; }
+function Goto_windowHTML(name_window2) { window.open(name_window2); }
+function ButtonHTML(text_button) { <button>{ text_button }</button> }
+function InputHTML(text_input) { <input> { text_input }</input> }
+function ExitCMD(status_ext) { exit(status_ext); }
+function File_buttonHTML() { <input type="file"></input> }
+function Output_pCMD(text_outputpcmd) { <p> { text_outputcmd } </p> }
